b648acb1f5e5ac004d007dcde32ee48319bf0be6
S1j4 <S1j4@DESKTOP-PR1G1RN.lan>
2021-10-14 16:52:56 +0200

    Förslag till player klassen

---
+++ RogueLike/src/main/java/Player/Player.java
@@ -0,0 +1,22 @@
+package Player;
+
+import java.util.ArrayList;
+
+public class Player {
+	private int health;
+	private int mana;
+	private int armor;
+	private PlayerClass playerClass;
+	private ArrayList<Equipment> playersEquipment = new ArrayList<>();
+	Player(PlayerClass playerClass) {
+		this.playerClass = playerClass;
+		this.health = playerClass.getHealth();
+		this.mana = playerClass.getMana();
+		this.armor = playerClass.getArmor();
+	}
+	
+	public void equipArmor(Equipment armorToEquip) {
+		this.playersEquipment.add(armorToEquip);
+	}
+	
+}
